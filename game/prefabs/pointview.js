58d8ce76d284f2b934b2a51747fd5f2fe083cc5c
Juho Leinonen <juholei@gmail.com>
2015-03-17 13:59:28 +0200

    Display correct index on PointView objects.

--- game/prefabs/pointview.js
+++ game/prefabs/pointview.js
@@ -1,11 +1,14 @@
 'use strict';
 
-var PointView = function(game, pointData) {
+var PointView = function(game, pointData, pointDataArray) {
   Phaser.Sprite.call(this, game, pointData.x * 0.75, pointData.y * 0.75, 'point', 0);
   this.PointData = pointData;
+  this.pointDataArray = pointDataArray;
   this.scale.setTo(0.5, 0.5);
   this.anchor.setTo(0.5, 0.5);
-  var text = game.add.text(0, 0, '1.', {font: '32px Arial', fill: 'white', align: 'center'});
+  var index = this.pointDataArray.indexOf(this.PointData);
+  var indexString = (index + 1) + '.';
+  var text = game.add.text(0, 0, indexString, {font: '32px Arial', fill: 'white', align: 'center'});
   text.anchor.setTo(0.5, 0.5);
   this.addChild(text);
 };
@@ -13,10 +16,6 @@ var PointView = function(game, pointData) {
 PointView.prototype = Object.create(Phaser.Sprite.prototype);
 PointView.prototype.constructor = PointView;
 
-PointView.prototype.update = function() {
-
-  // write your prefab's specific update code here
-
-};
+PointView.prototype.update = function() {};
 
 module.exports = PointView;
--- game/states/editor.js
+++ game/states/editor.js
@@ -15,9 +15,9 @@ Editor.prototype = {
   update: function() {
   },
   clickListener: function(sprite, pointer) {
-    var newPoint = new GameDataCreator.GamePoint(this.screenToGameCoordinate(pointer.x),  this.screenToGameCoordinate(pointer.y), 'unvisited');
+    var newPoint = new GameDataCreator.GamePoint(this.scaleUp(pointer.x), this.scaleUp(pointer.y), 'unvisited');
     this.game.data.points.push(newPoint);
-    var pointSprite = new PointView(this.game, newPoint);
+    var pointSprite = new PointView(this.game, newPoint, this.game.data.points);
     this.game.add.existing(pointSprite);
     this.sprites.add(pointSprite);
     this.updatePreviewText();
@@ -26,7 +26,9 @@ Editor.prototype = {
     var textArea = window.document.getElementById('outputJSON');
     textArea.value = JSON.stringify(this.game.data, null, 2);
   },
-  screenToGameCoordinate: function(number) {
+  //Scales up point related numbers from the 0.75 scale of the displayed game area
+  //to the correct game area size used in the game
+  scaleUp: function(number) {
     var scaleCorrectedNumber = (number / 3) * 4;
     return Math.floor(scaleCorrectedNumber);
   }
