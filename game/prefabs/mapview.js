99e1f314e2f270fe9f2ada709d1acac86529caed
Juho Leinonen <juholei@gmail.com>
2015-03-22 15:08:01 +0200

    Load background image in MapView based on gamedata if set there. Refactor MapView a bit.

--- game/prefabs/mapview.js
+++ game/prefabs/mapview.js
@@ -13,7 +13,10 @@ var MapView = function(game) {
   this.pointViewSprites = this.game.add.group();
   this.addChild(this.pointViewSprites);
 
-  this.initializeFromGameData();
+  this.startPointViewSprite = this.initializeMajorPointView(this.game.data.startPoint, 0);
+  this.endPointViewSprite = this.initializeMajorPointView(this.game.data.endPoint, 1);
+  this.initializePointViewsFromGameData();
+  this.loadBackgroundImage();
 };
 
 MapView.prototype = Object.create(Phaser.Image.prototype);
@@ -53,27 +56,33 @@ MapView.prototype.updatePointViews = function() {
     }, this);
 };
 
-MapView.prototype.initializeFromGameData = function() {
-  if (this.game.data.startPoint.x !== undefined) {
-    this.startPointViewSprite = new MajorPointView(this.game, this.game.data.startPoint, 0);
-    this.addChild(this.startPointViewSprite);
-  } else {
-    this.startPointViewSprite = null;
-    console.log('n');
-  }
-
-  if (this.game.data.endPoint.x !== undefined) {
-    this.endPointViewSprite = new MajorPointView(this.game, this.game.data.endPoint, 1);
-    this.addChild(this.endPointViewSprite);
-
-  } else {
-    this.endPointViewSprite = null;
-  }
-
+MapView.prototype.initializePointViewsFromGameData = function() {
   var pointDataArray = this.game.data.points;
-
   for (var i = 0; i < pointDataArray.length; i++) {
     this.pointViewSprites.add(new PointView(this.game, pointDataArray[i], pointDataArray));
+  }
+};
+
+MapView.prototype.initializeMajorPointView = function(data, frame) {
+  var view = null;
+  if (data.x !== undefined) {
+    view = new MajorPointView(this.game, data, frame);
+    this.addChild(view);
+  }
+  return view;
+};
+
+MapView.prototype.loadBackgroundImage = function() {
+  var image = new Image();
+  var sprite = this;
+  image.onload = function addImageToSprite() {
+    sprite.displayImage.loadTexture(new PIXI.Texture(new PIXI.BaseTexture(image, PIXI.scaleModes.DEFAULT)));
+    console.log('Image loaded');
+    sprite.displayImage.width = 1024;
+    sprite.displayImage.height = 768;
+  };
+  if (this.game.data.image !== undefined) {
+    image.src = this.game.data.image;
   }
 };
 
