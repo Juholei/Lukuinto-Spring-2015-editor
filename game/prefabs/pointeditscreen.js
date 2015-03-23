f6fba4ad5e44bb5ba929b1dc09312ba369c3f546
Juho Leinonen <juholei@gmail.com>
2015-03-23 10:06:21 +0200

    Initial implementation of text area in PointEditScreen.

--- game/prefabs/pointeditscreen.js
+++ game/prefabs/pointeditscreen.js
@@ -1,16 +1,23 @@
 'use strict';
 
 var PointEditScreen = function(game, pointData) {
-  Phaser.Image.call(this, game, 500, 100, 'point-edit-screen');
+  Phaser.Image.call(this, game, 600, 50, 'point-edit-screen');
   this.pointData = pointData;
+  this.scale.setTo(1, 0.75);
+  this.addQuestionInputBox();
 };
 
 PointEditScreen.prototype = Object.create(Phaser.Image.prototype);
 PointEditScreen.prototype.constructor = PointEditScreen;
 
 PointEditScreen.prototype.update = function() {
+};
 
-  // write your prefab's specific update code here
+PointEditScreen.prototype.addQuestionInputBox = function() {
+  var parentDiv = window.document.getElementById('lukuinto-spring-2015-editor');
+  var textBox = window.document.createElement('textarea');
+  textBox.className = 'textbox';
+  parentDiv.appendChild(textBox);
 
 };
 
