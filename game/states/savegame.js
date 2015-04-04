d260066d862e882b3a7832d10e727a25f950f86a
Juho Leinonen <juholei@gmail.com>
2015-04-04 14:06:15 +0300

    Upload files in order and after all of them have finished send game json to server.

--- game/states/savegame.js
+++ game/states/savegame.js
@@ -20,50 +20,60 @@ SaveGame.prototype = {
   },
   uploadGameData: function() {
     console.log('Uploading');
+    var stack = [];
     var gameData = this.game.data;
     if (gameData.image !== null && gameData.image !== undefined) {
-      this.uploadImage(this.game.data);
+      stack.push(this.game.data);
     }
     for (var i = 0; i < gameData.points.length; i++) {
       var pointData = gameData.points[i];
       for (var j = 0; j < pointData.tasks.length; j++) {
         var task = pointData.tasks[j];
         if (task.image !== null && gameData.image !== undefined) {
-          this.uploadImage(task);
+          stack.push(task);
         }
       }
     }
-    this.uploadGameJSON();
+    this.uploadImage(stack);
+    // this.uploadGameJSON();
   },
   //Uploads the image which ObjectURL is at
   //dataObject.image and after uploading,
   //changes dataObject.image to image id
   //in the database.
-  uploadImage: function(dataObject) {
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', dataObject.image);
-    xhr.responseType = 'blob';
-    xhr.onload = function() {
-      if (this.status === 200) {
-        var file = this.response;
-        var formData = new FormData();
-        formData.append('file', file);
-        var request = new XMLHttpRequest();
-        request.open('POST', '/upload');
-        request.onload = function() {
-          if (request.status === 200) {
-            console.log('Uploaded');
-            console.log(request.responseText);
-            var responseObject = JSON.parse(request.responseText);
-            dataObject.image = responseObject.id;
-          } else {
-            console.log(request.status + 'occurred uploading your file.');
-          }
-        };
-        request.send(formData);
-      }
-    };
-    xhr.send();
+  uploadImage: function(stack) {
+    var dataObject = stack.pop();
+
+    if (dataObject === undefined) {
+      this.uploadGameJSON();
+    } else {
+      var xhr = new XMLHttpRequest();
+      xhr.open('GET', dataObject.image);
+      xhr.responseType = 'blob';
+      var self = this;
+      xhr.onload = function() {
+        if (this.status === 200) {
+          var file = this.response;
+          var formData = new FormData();
+          formData.append('file', file);
+          var request = new XMLHttpRequest();
+          request.open('POST', '/upload');
+          request.onload = function() {
+            if (request.status === 200) {
+              console.log('Uploaded');
+              console.log(request.responseText);
+              var responseObject = JSON.parse(request.responseText);
+              dataObject.image = responseObject.id;
+              self.uploadImage(stack);
+            } else {
+              console.log(request.status + 'occurred uploading your file.');
+            }
+          };
+          request.send(formData);
+        }
+      };
+      xhr.send();
+    }
   },
   uploadGameJSON: function() {
     var request = new XMLHttpRequest();
